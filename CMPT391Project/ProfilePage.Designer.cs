a0e7f14efd3857a83f8adbaf4fad3f84d3389953
JustinN9 <justinnorton99@icloud.com>
2024-02-11 19:39:23 -0700

    Basics of profile functionality

--- CMPT391Project/ProfilePage.Designer.cs
+++ CMPT391Project/ProfilePage.Designer.cs
@@ -28,55 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.nameLabel = new System.Windows.Forms.Label();
             this.emailLabel = new System.Windows.Forms.Label();
             this.emailTextBox = new System.Windows.Forms.TextBox();
             this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(89, 88);
+            this.nameLabel.Location = new System.Drawing.Point(119, 108);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.Size = new System.Drawing.Size(80, 17);
             this.nameLabel.TabIndex = 0;
-            this.nameLabel.Text = "Name:";
+            this.nameLabel.Text = "First Name:";
             this.nameLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // emailLabel
             // 
             this.emailLabel.AutoSize = true;
-            this.emailLabel.Location = new System.Drawing.Point(93, 169);
+            this.emailLabel.Location = new System.Drawing.Point(119, 190);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(34, 13);
+            this.emailLabel.Size = new System.Drawing.Size(46, 17);
             this.emailLabel.TabIndex = 1;
-            this.emailLabel.Text = "email:";
+            this.emailLabel.Text = "Email:";
+            this.emailLabel.Click += new System.EventHandler(this.emailLabel_Click);
             // 
             // emailTextBox
             // 
-            this.emailTextBox.Location = new System.Drawing.Point(145, 162);
+            this.emailTextBox.Location = new System.Drawing.Point(394, 187);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(203, 20);
+            this.emailTextBox.Size = new System.Drawing.Size(269, 22);
             this.emailTextBox.TabIndex = 2;
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(145, 81);
+            this.nameTextBox.Location = new System.Drawing.Point(394, 105);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(203, 20);
+            this.nameTextBox.Size = new System.Drawing.Size(269, 22);
             this.nameTextBox.TabIndex = 2;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(119, 148);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(80, 17);
+            this.lastNameLabel.TabIndex = 3;
+            this.lastNameLabel.Text = "Last Name:";
+            this.lastNameLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Location = new System.Drawing.Point(119, 232);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(107, 17);
+            this.phoneNumberLabel.TabIndex = 4;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(394, 145);
+            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(269, 22);
+            this.lastNameTextBox.TabIndex = 9;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(394, 229);
+            this.phoneNumberTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(269, 22);
+            this.phoneNumberTextBox.TabIndex = 10;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(394, 271);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(269, 22);
+            this.passwordTextBox.TabIndex = 11;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(588, 381);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 12;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
             // 
             // ProfilePage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.lastNameLabel);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.nameLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ProfilePage";
-            this.Size = new System.Drawing.Size(635, 476);
+            this.Size = new System.Drawing.Size(847, 586);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,5 +185,14 @@
         private System.Windows.Forms.Label emailLabel;
         private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button updateButton;
     }
 }
--- CMPT391Project/ProfilePage.cs
+++ CMPT391Project/ProfilePage.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,9 +17,62 @@ namespace CMPT391Project
         public ProfilePage()
         {
             InitializeComponent();
+
+            var sqlConn = ConfigurationManager.ConnectionStrings["myConnStr"].ConnectionString;
+
+            // Default local host database with name CMPT391Database
+            using (SqlConnection connection = new SqlConnection(sqlConn))
+            {
+                try
+                {
+                    connection.Open();
+                    // Using the check login procedure
+                    using (SqlCommand cmd = new SqlCommand("show_user", connection))
+                    //using (SqlCommand cmd = new SqlCommand("check_login", conn))
+                    {
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Get inputted username/password
+                        //cmd.Parameters.AddWithValue("@username", userName.text);
+
+                        // Parameter for retrieving return value
+                        SqlParameter returnedParam = new SqlParameter("@ReturnValue", SqlDbType.Int);
+                        returnedParam.Direction = ParameterDirection.ReturnValue;
+                        cmd.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+
+                        cmd.ExecuteNonQuery();
+
+
+                        returnedValue = (int)cmd.Parameters["@ReturnValue"].Value;
+
+
+                    }
+                }
+                catch (SqlException exception)
+                {
+                    System.Diagnostics.Debug.WriteLine(exception.Message);
+                }
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
