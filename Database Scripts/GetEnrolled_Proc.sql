8025d9b5a3e575916841bd0ee21e30a4318b7e51
Olasubomi O. Badiru <badirusubomi@icloud.com>
2024-02-14 10:26:11 -0700

    added display messages for profile update, modified etenolled procedure

--- Database Scripts/GetEnrolled_Proc.sql
+++ Database Scripts/GetEnrolled_Proc.sql
@@ -4,28 +4,6 @@ alter PROCEDURE show_enrolled_classes
 	@year NVARCHAR(8)
 AS
 BEGIN
-   (SELECT c.courseName, t.sem, t.year, s.secID, ts.day, ts.startTime, ts.endTime
-   FROM Course c, Taken t, Section s, Timeslot ts
-   WHERE   t.studentID = @username 
-   AND t.courseID = c.courseID
-   AND t.secID = s.secID
-   AND s.courseID = c.courseID
-   AND s.timeslotID = ts.timeslotID
-   AND UPPER(t.sem) = UPPER(@sem)
-   AND t.year = @year)
-   
-   
-   
-END
-go
-
-
-alter PROCEDURE show_enrolled_classes
-    @username int,
-    @sem NVARCHAR(32),
-	@year NVARCHAR(8)
-AS
-BEGIN
    (SELECT c.courseID, c.courseName, t.sem, t.year, s.secID, ts.day, ts.startTime, ts.endTime
    FROM
     Section s
@@ -35,8 +13,7 @@ JOIN
     Course c ON s.courseID = c.courseID
 JOIN
     Timeslot ts ON s.timeslotID = ts.timeslotID
-   WHERE   t.studentID = @username and t.sem = @sem and t.year = @year)
-   
+   WHERE   t.studentID = @username and t.sem = @sem and t.year = @year and (t.progress = 2 or t.progress = 3))
    
 END
 
