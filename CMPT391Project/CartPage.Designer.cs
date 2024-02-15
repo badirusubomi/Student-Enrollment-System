eaa06dd824cce9027b19dfdfb5ba5b5b4c3c8a26
Eric Carstensen <37225222+E-Carstensen@users.noreply.github.com>
2024-02-14 20:30:33 -0700

    added enroll button with hard coded values

--- CMPT391Project/CartPage.Designer.cs
+++ CMPT391Project/CartPage.Designer.cs
@@ -30,45 +30,50 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.enrollButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(109, 99);
+            this.dataGridView1.Location = new System.Drawing.Point(164, 152);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(427, 258);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 397);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(109, 54);
+            this.label1.Location = new System.Drawing.Point(164, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.Size = new System.Drawing.Size(218, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Courses in your shopping cart";
             // 
-            // enrollButton
+            // button1
             // 
-            this.enrollButton.Location = new System.Drawing.Point(461, 404);
-            this.enrollButton.Name = "enrollButton";
-            this.enrollButton.Size = new System.Drawing.Size(75, 23);
-            this.enrollButton.TabIndex = 2;
-            this.enrollButton.Text = "Enrol";
-            this.enrollButton.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(723, 603);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Enroll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // CartPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.enrollButton);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CartPage";
-            this.Size = new System.Drawing.Size(635, 476);
+            this.Size = new System.Drawing.Size(952, 732);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -79,6 +84,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button enrollButton;
+        private System.Windows.Forms.Button button1;
     }
 }
--- CMPT391Project/CartPage.cs
+++ CMPT391Project/CartPage.cs
@@ -1,55 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CMPT391Project
 {
     public partial class CartPage : UserControl
     {
+
+        int cID = 1;
+        int secID = 1;
+        string sqlConn = ConfigurationManager.ConnectionStrings["myConnStr"].ConnectionString;
+
+
         public CartPage()
         {
             InitializeComponent();
         }
-    }
 
-    private void showCart()
-    {
-
-        //var sqlConn = ConfigurationManager.ConnectionStrings["myConnStr"].ConnectionString;
-
-        // Default local host database with name CMPT391Database
-        using (SqlConnection conn = new SqlConnection(sqlConn))
+        private void button1_Click(object sender, EventArgs e)
         {
-            try
+            int returnedValue = 0;
+            using (SqlConnection conn = new SqlConnection(sqlConn))
             {
-                conn.Open();
-                // Using the check login procedure
-                using (SqlCommand cmd = new SqlCommand("show_cart", conn))
+                try
                 {
+                    conn.Open();
+                    // Using the check login procedure
+                    using (SqlCommand cmd = new SqlCommand("enroll_class", conn))
+                    {
 
-                    cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandType = CommandType.StoredProcedure;
 
 
-                    // Get inputted semester and year
-                    cmd.Parameters.AddWithValue("@studentId", Program.globalString);
-                    
-                    adpt = new SqlDataAdapter(cmd);
-                    dt = new DataTable();
-                    adpt.Fill(dt);
-                    dataGridView1.DataSource = dt;
-                    conn.Close();
+                        // Get inputted semester and year
+                        cmd.Parameters.AddWithValue("@studentID", Program.globalString);
+
+                        cmd.Parameters.AddWithValue("@courseID", cID);
+                        cmd.Parameters.AddWithValue("@sectionID", secID);
+                        cmd.Parameters.AddWithValue("@semester", "FALL");
+                        cmd.Parameters.AddWithValue("@year", "2024");
+
+
+
+                        SqlParameter returnedParam = new SqlParameter("@ReturnValue", SqlDbType.Int);
+                        returnedParam.Direction = ParameterDirection.ReturnValue;
+                        cmd.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+                        cmd.ExecuteNonQuery();
+
+                        returnedValue = (int)cmd.Parameters["@ReturnValue"].Value;
+
+                    }
+                }
+                catch (SqlException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+                catch (IndexOutOfRangeException exception2)
+                {
+                    MessageBox.Show(exception2.Message);
                 }
             }
-            catch (SqlException exception)
-            {
 
+
+            if (returnedValue < 0)
+            {
+                MessageBox.Show("Error - " + returnedValue);
             }
+            else
+            {
+                MessageBox.Show("Successfully Enrolled");
+            }
+
+
         }
     }
+
+
+   
+ 
 }
